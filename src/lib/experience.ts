97754cdd922e36edeea95c0e2e88974ae4192538
LorryGailius <lauris.gailius@gmail.com>
2024-01-04 00:34:21 +0200

    Added experiences div

---
+++ src/lib/experience.ts
@@ -0,0 +1,58 @@
+export const experience = [
+  {
+    title: "Azure",
+    link: "https://azure.microsoft.com/en-us",
+  },
+  {
+    title: "MAUI",
+    link: "https://dotnet.microsoft.com/en-us/apps/maui",
+  },
+  {
+    title: "Blazor",
+    link: "https://dotnet.microsoft.com/en-us/apps/aspnet/web-apps/blazor",
+  },
+  {
+    title: "ASP.NET",
+    link: "https://www.asp.net/",
+  },
+  {
+    title: "SignalR",
+    link: "https://dotnet.microsoft.com/en-us/apps/aspnet/signalr",
+  },
+  {
+    title: "JavaFX",
+    link: "https://openjfx.io/",
+  },
+  {
+    title: "JQuery",
+    link: "https://jquery.com/",
+  },
+  {
+    title: "React",
+    link: "https://react.dev/",
+  },
+  {
+    title: "NextJS",
+    link: "https://nextjs.org/",
+  },
+  {
+    title: "Tailwind CSS",
+    link: "https://tailwindcss.com/",
+  },
+  {
+    title: "Astro",
+    link: "https://astro.build/",
+  },
+  {
+    title: "PostgreSQL",
+    link: "https://www.postgresql.org/",
+  },
+  {
+    title: "MS SQL",
+    link: "https://www.microsoft.com/en-us/sql-server/",
+  },
+  {
+    title: "Firebase",
+    link: "https://firebase.google.com/",
+  },
+];
