a15a8f2d7dcca661083804df966ecff55dcc7581
LorryGailius <lauris.gailius@gmail.com>
2024-01-05 01:15:57 +0200

    Add astro-robots-txt integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-
 import icon from "astro-icon";
+
+import robotsTxt from "astro-robots-txt";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://lorrygailius.github.io",
-  integrations: [tailwind(), icon()],
+  integrations: [tailwind(), icon(), robotsTxt()]
 });
