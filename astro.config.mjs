fe0bd5a6b7fed8d8f55a846d7a67cd096c5fc63b
LorryGailius <lauris.gailius@gmail.com>
2024-01-05 02:08:31 +0200

    Add sitemap integration to astro.config.mjs and package.json

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
-
 import robotsTxt from "astro-robots-txt";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://lorrygailius.github.io",
-  integrations: [tailwind(), icon(), robotsTxt()]
+  integrations: [tailwind(), icon(), robotsTxt(), sitemap()]
 });
